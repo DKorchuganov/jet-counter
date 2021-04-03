561d3a94621cebddd894b0fabaeaf3dbb2400839
Denis Korchuganov <DKorchuganov@yandex.ru>
2021-04-03 12:40:31 +0400

    MultiCounterSetupScreen design is changed

--- app/src/main/java/com/jvmlab/android/jetcounter/composables/MultiCounterSetup.kt
+++ app/src/main/java/com/jvmlab/android/jetcounter/composables/MultiCounterSetup.kt
@@ -3,15 +3,16 @@ package com.jvmlab.android.jetcounter.composables
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.jvmlab.android.jetcounter.models.MultiCounterSetupModel
 
 
@@ -20,16 +21,20 @@ fun MultiCounterSetup(model: MultiCounterSetupModel, navigateToCounter: () -> Un
     val counterTitle: String by model.counterTitleLive.observeAsState("")
 
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         OutlinedTextField(
+            modifier = Modifier.padding(8.dp),
             value = counterTitle,
             onValueChange = { model.textFieldEventHandler(it) },
             label = { Text("Counter title") }
         )
+        Divider()
         Button(
+            modifier = Modifier.padding(8.dp),
             onClick = {
                 model.buttonEventHandler()
                 navigateToCounter()
