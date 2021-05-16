184e8db599f6e831e95a1982883bb44afffca43a
Denis Korchuganov <dkorchuganov@yandex.ru>
2021-05-16 18:09:58 +0300

    Show details icon is added to SimpleCounterCard

--- app/src/main/java/com/jvmlab/android/jetcounter/composables/SimpleCounterCard.kt
+++ app/src/main/java/com/jvmlab/android/jetcounter/composables/SimpleCounterCard.kt
@@ -1,18 +1,18 @@
 package com.jvmlab.android.jetcounter.composables
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ExpandMore
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.jvmlab.android.jetcounter.R
 import com.jvmlab.android.jetcounter.models.SimpleCounterModel
 
 
@@ -20,20 +20,32 @@ import com.jvmlab.android.jetcounter.models.SimpleCounterModel
 fun SimpleCounterCard(model: SimpleCounterModel, onClick: () -> Unit) {
     val countString: String by model.countStringLive.observeAsState("")
     Card(
-        modifier =
-        Modifier
-            .padding(16.dp)
-            .fillMaxWidth()
-            .clickable(onClick = onClick),
+        modifier = Modifier.padding(bottom = 16.dp),
         elevation = 4.dp
     ) {
-        Row(
-            modifier = Modifier.padding(8.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(text = model.title, style = MaterialTheme.typography.h5)
-            Text(text = countString, style = MaterialTheme.typography.h5)
+        Column {
+            Row(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth()
+                    .clickable(onClick = onClick),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = model.title, style = MaterialTheme.typography.h5)
+                Text(text = countString, style = MaterialTheme.typography.h5)
+            }
+            Divider(modifier = Modifier.padding(horizontal = 8.dp))
+            Row(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth()
+                    .clickable(onClick = { /*TODO*/ }),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = stringResource(R.string.show_details))
+                Icon(Icons.Outlined.ExpandMore, null)
+            }
         }
-
     }
 }
--- app/src/main/java/com/jvmlab/android/jetcounter/composables/SimpleCounterList.kt
+++ app/src/main/java/com/jvmlab/android/jetcounter/composables/SimpleCounterList.kt
@@ -2,10 +2,12 @@ package com.jvmlab.android.jetcounter.composables
 
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.jvmlab.android.jetcounter.models.SimpleCounterModel
 
 
@@ -15,10 +17,13 @@ fun SimpleCounterList(
     navigateByIndex: (Int) -> Unit
 ) {
     Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-        for (index in 0..simpleCounterModelList.lastIndex) {
-            SimpleCounterCard(simpleCounterModelList[index]) {
-                navigateByIndex(index)
+        Column(modifier = Modifier.padding(16.dp)) {
+            for (index in 0..simpleCounterModelList.lastIndex) {
+                SimpleCounterCard(simpleCounterModelList[index]) {
+                    navigateByIndex(index)
+                }
             }
         }
+
     }
 }
